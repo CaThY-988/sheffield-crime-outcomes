0a0566d0f82575539d945e1d7b40b0776bccf2bf
CaThY-988 <50583154+CaThY-988@users.noreply.github.com>
2026-04-13 21:08:29 +0000

    update dbt after chages to load dbt script

--- dbt_police_data/models/intermediate/int_police_crimes.sql
+++ dbt_police_data/models/intermediate/int_police_crimes.sql
@@ -1,4 +1,19 @@
-{{ config(materialized='view') }}
+with parsed as (
+
+    select
+        *,
+        from_json(
+            location,
+            'latitude STRING, longitude STRING, street STRUCT<id: BIGINT, name: STRING>'
+        ) as location_struct,
+        from_json(
+            outcome_status,
+            'category STRING, date STRING'
+        ) as outcome_struct
+
+    from {{ ref('stg_police_crimes') }}
+
+)
 
 select
     crime_id,
@@ -8,13 +23,13 @@ select
     location_type,
     location_subtype,
     context,
+    
+    location_struct.latitude as crime_latitude,
+    location_struct.longitude as crime_longitude,
+    location_struct.street.id as crime_street_id,
+    location_struct.street.name as crime_street_name,
 
-    location.latitude as crime_latitude,
-    location.longitude as crime_longitude,
-    location.street.id as crime_street_id,
-    location.street.name as crime_street_name,
+    outcome_struct.category as latest_outcome_category,
+    outcome_struct.date as latest_outcome_date
 
-    outcome_status.category as latest_outcome_category,
-    outcome_status.date as latest_outcome_date
-
-from {{ ref('stg_police_crimes') }}
+from parsed
--- dbt_police_data/models/intermediate/int_police_outcomes.sql
+++ dbt_police_data/models/intermediate/int_police_outcomes.sql
@@ -1,22 +1,55 @@
 {{ config(materialized='view') }}
 
+with parsed as (
+
+    select
+        *,
+        from_json(
+            category,
+            'code STRING, name STRING'
+        ) as category_struct,
+        from_json(
+            crime,
+            '''
+            id BIGINT,
+            persistent_id STRING,
+            category STRING,
+            location_type STRING,
+            location_subtype STRING,
+            context STRING,
+            month STRING,
+            location STRUCT<
+                latitude: STRING,
+                longitude: STRING,
+                street: STRUCT<
+                    id: BIGINT,
+                    name: STRING
+                >
+            >
+            '''
+        ) as crime_struct
+
+    from {{ ref('stg_police_outcomes') }}
+
+)
+
 select
-    category.code as outcome_category_code,
-    category.name as outcome_category_name,
+    category_struct.code as outcome_category_code,
+    category_struct.name as outcome_category_name,
     outcome_date,
     person_id,
 
-    crime.id as crime_id,
-    crime.persistent_id as crime_persistent_id,
-    crime.category as crime_category,
-    crime.location_type as crime_location_type,
-    crime.location_subtype as crime_location_subtype,
-    crime.context as crime_context,
-    crime.month as crime_month,
+    crime_struct.id as crime_id,
+    crime_struct.persistent_id as crime_persistent_id,
+    crime_struct.category as crime_category,
+    crime_struct.location_type as crime_location_type,
+    crime_struct.location_subtype as crime_location_subtype,
+    crime_struct.context as crime_context,
+    crime_struct.month as crime_month,
 
-    crime.location.latitude as crime_latitude,
-    crime.location.longitude as crime_longitude,
-    crime.location.street.id as crime_street_id,
-    crime.location.street.name as crime_street_name
+    crime_struct.location.latitude as crime_latitude,
+    crime_struct.location.longitude as crime_longitude,
+    crime_struct.location.street.id as crime_street_id,
+    crime_struct.location.street.name as crime_street_name
 
-from {{ ref('stg_police_outcomes') }}
+from parsed
